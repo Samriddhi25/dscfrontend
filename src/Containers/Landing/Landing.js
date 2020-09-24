84dcfe7cc540b091cd96a21d0ac766b587afcbd1
aashishpeepra <ironhack082327@gmail.com>
2020-09-24 11:18:56 +0530

    Added the structure -1

---
+++ src/Containers/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Landing.css";
+
+
+export default class Landing extends React.Component{
+    state={
+
+    }
+    render(){
+        return(
+            <h1>Landing Page</h1>
+        )
+    }
+}
